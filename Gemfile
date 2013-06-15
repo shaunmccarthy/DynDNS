a4ae2eacbdfbc3beb25725fb00e173828de795ff
Shaun McCarthy <git@shaunmccarthy.com>
2013-06-14 23:59:41 -0400

    Tidied up files, added comments and documentation
    Just in case someone stumbled upon this repository and finds it helpful
    :)

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source "https://rubygems.org"
-gem 'psych'
+# You might need to comment this out to run locally, since Heroku
+# will use Syck, you need to tell it to use psych, but if you are 
+# running on windows, this causes problems if you don't have the
+# dev kit installed
+# gem "psych", "~> 1.3.4"
 gem 'mechanize'
 gem 'tlsmail'
--- dyndns_login.rb
+++ dyndns_login.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
- 
+
+# Required to automagically request the other gems at runtime 
 require 'rubygems'
 require 'bundler/setup'
 
+# For browser automation
 require 'mechanize'
+
+# To send out logging emails
 require 'tlsmail'
 require 'time'
 
@@ -20,29 +24,39 @@ error_email_to = ENV['error_email_to']
 
 begin
 
+  # connect to the dyndns site with a pretend browser and go to the login page
+  # based off of https://gist.github.com/meise/5585311
   agent = Mechanize.new
   agent.user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'
  
   login_page = agent.get('https://account.dyn.com/entrance/')
- 
+
+  # login 
   result_login = login_page.form_with(action: '/entrance/') do |form|
     form.username = dyndns_username
     form.password = dyndns_password
   end.click_button
 
+  # Check we succeeded
   result = result_login.body
 
-  if ( result =~ /<span>Welcome\&nbsp;<b>slyrp<\/b><\/span>/ )
+  if ( result =~ /<span>Welcome\&nbsp;<b>#{dyndns_username}<\/b><\/span>/ )
     puts "Logged In"
   else
+    # Throw an error
     puts "Unable to login"
     raise "Failed to Login"
   end
 
+# If we get an error, try and send an email to error_email_to telling them that something
+# went wrong
 rescue Exception => e 
+
+  # If we don't have a email then just dump to console
   if error_email_from.nil?
-    puts "Not sending error email since error_email_from is not set"
+    puts "Not sending error email since error_email_from is not set:\n#{e.message}\n#{e.backtrace.inspect}"
   else
+    # Create the email
     content = <<EOF
 From: #{error_email_from}
 To: #{error_email_to}
@@ -54,8 +68,10 @@ Error is...
 #{e.backtrace.inspect}
 EOF
 
+    # Debug to console
     print content
- 
+
+    # Send the email 
     Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)  
     Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', error_email_from, error_email_from_password, :login) do |smtp| 
       smtp.send_message(content, error_email_from, error_email_to)
